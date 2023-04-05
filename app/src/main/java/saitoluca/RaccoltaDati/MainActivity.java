b8417f1811840162f8486224658c3e9cad07e8b2
SaitoLuca <luca.saito@isitgo.it>
2023-04-05 22:10:40 +0200

    attività dei permessi
    creazione di un attività predisposta alla richista dei permessi necessari (non sono tutti presenti)

--- app/src/main/java/saitoluca/RaccoltaDati/MainActivity.java
+++ app/src/main/java/saitoluca/RaccoltaDati/MainActivity.java
@@ -1,13 +1,35 @@
 package saitoluca.RaccoltaDati;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    protected static final String[] perms = {android.Manifest.permission.PACKAGE_USAGE_STATS,android.Manifest.permission.ACTIVITY_RECOGNITION};
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        boolean arePermsSetted = true;
+        for (String perm : perms) {
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), perm) != PackageManager.PERMISSION_GRANTED){
+                arePermsSetted = false;
+                break;
+            }
+        }
+        if(!arePermsSetted) {
+            Intent intent = new Intent(this, PermissionsActivity.class);
+            this.startActivity(intent);
+        }
     }
 }
