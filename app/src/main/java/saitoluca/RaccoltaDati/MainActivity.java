dd8229e79b470426578c4e8428c011c40fd75f83
SaitoLuca <luca.saito@isitgo.it>
2023-04-06 11:57:24 +0200

    richiesta permessi
    siamo ora in grado di richiedere i permessi che ci servono (non sono tutti)

--- app/src/main/java/saitoluca/RaccoltaDati/MainActivity.java
+++ app/src/main/java/saitoluca/RaccoltaDati/MainActivity.java
@@ -1,16 +1,21 @@
 package saitoluca.RaccoltaDati;
 
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.app.AppOpsManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     protected static final String[] perms = {android.Manifest.permission.PACKAGE_USAGE_STATS,android.Manifest.permission.ACTIVITY_RECOGNITION};
     private static final String TAG = "MainActivity";
+    protected static boolean arePermsSetted=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +25,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        boolean arePermsSetted = true;
-        for (String perm : perms) {
-            if(ActivityCompat.checkSelfPermission(getApplicationContext(), perm) != PackageManager.PERMISSION_GRANTED){
-                arePermsSetted = false;
-                break;
+        arePermsSetted = true;
+        if (getGrantStatus()){
+            arePermsSetted = false;
+        }else {
+            for (int i = 1; i < perms.length; i++) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), perms[i]) != PackageManager.PERMISSION_GRANTED) {
+                    arePermsSetted = false;
+                    break;
+                }
             }
         }
         if(!arePermsSetted) {
             Intent intent = new Intent(this, PermissionsActivity.class);
             this.startActivity(intent);
+        }
+    }
+
+    //method to check APP_USAGE permission
+    private boolean getGrantStatus() {
+        AppOpsManager appOps = (AppOpsManager) getApplicationContext()
+                .getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.unsafeCheckOpNoThrow(OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getApplicationContext().getPackageName());
+        if (mode == AppOpsManager.MODE_DEFAULT) {
+            return (getApplicationContext().checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) != PackageManager.PERMISSION_GRANTED);
+        } else {
+            return (mode != MODE_ALLOWED);
         }
     }
 }
--- app/src/main/java/saitoluca/RaccoltaDati/PermissionsActivity.java
+++ app/src/main/java/saitoluca/RaccoltaDati/PermissionsActivity.java
@@ -1,13 +1,25 @@
 package saitoluca.RaccoltaDati;
 
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+import static saitoluca.RaccoltaDati.MainActivity.arePermsSetted;
+import static saitoluca.RaccoltaDati.MainActivity.perms;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
+import android.provider.Settings;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class PermissionsActivity extends AppCompatActivity {
+    private static final String TAG = "PermissionsActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +29,54 @@ public class PermissionsActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        updateView();
         Button enableBtn = findViewById(R.id.btnEnable);
-        enableBtn.setOnClickListener(view -> finish());
+        enableBtn.setOnClickListener(view -> permissions());
+    }
+
+    private void updateView() {
+        arePermsSetted=true;
+        TextView[] views = {findViewById(R.id.txtPerm1),findViewById(R.id.txtPerm2),findViewById(R.id.txtPerm3)};
+        if (getGrantStatus()){
+            views[0].setTextColor(getColor(R.color.redTxt));
+            arePermsSetted=false;
+        }else{
+            views[0].setTextColor(getColor(R.color.greenTxt));
+        }
+        for (int i=1;i<perms.length;i++) {
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), perms[i]) != PackageManager.PERMISSION_GRANTED){
+                views[i].setTextColor(getColor(R.color.redTxt));
+                arePermsSetted=false;
+            }else{
+                views[i].setTextColor(getColor(R.color.greenTxt));
+            }
+        }
+    }
+
+    private void permissions() {
+        if(arePermsSetted){
+            this.finish();
+        }
+        if (getGrantStatus()){
+            this.startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+        }
+        for (int i=1; i<perms.length;i++){
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), perms[i]) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, perms,1313);
+            }
+        }
+        updateView();
+    }
+    //method to check APP_USAGE permission
+    private boolean getGrantStatus() {
+        AppOpsManager appOps = (AppOpsManager) getApplicationContext()
+                .getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.unsafeCheckOpNoThrow(OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getApplicationContext().getPackageName());
+        if (mode == AppOpsManager.MODE_DEFAULT) {
+            return (getApplicationContext().checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) != PackageManager.PERMISSION_GRANTED);
+        } else {
+            return (mode != MODE_ALLOWED);
+        }
     }
 }
